a86d3635a3756a574d9f712350bafb30f0c2029c
David <dgx.arnold@gmail.com>
2024-09-16 21:36:34 +0200

    style: ruff format

--- payments/payment_gateways/doctype/razorpay_settings/razorpay_settings.py
+++ payments/payment_gateways/doctype/razorpay_settings/razorpay_settings.py
@@ -382,8 +382,8 @@ class RazorpaySettings(Document):
 
 	def authorize_payment(self):
 		"""
-		An authorization is performed when user’s payment details are successfully authenticated by the bank.
-		The money is deducted from the customer’s account, but will not be transferred to the merchant’s account
+		An authorization is performed when user's payment details are successfully authenticated by the bank.
+		The money is deducted from the customer's account, but will not be transferred to the merchant's account
 		until it is explicitly captured by merchant.
 		"""
 		data = json.loads(self.integration_request.data)
@@ -472,7 +472,7 @@ class RazorpaySettings(Document):
 		settings = self.get_settings({})
 
 		try:
-			resp = make_post_request(
+			make_post_request(
 				f"https://api.razorpay.com/v1/subscriptions/{subscription_id}/cancel",
 				auth=(settings.api_key, settings.api_secret),
 			)
